c42488b3d10a3ffd689cd7fd5a55b57f71d050e8
DmitriyShalaev <shalaevdmitriy99@gmail.com>
2020-06-15 16:06:14 +0300

    code comment 2

--- controller/adapter.controller.js
+++ controller/adapter.controller.js
@@ -5,18 +5,23 @@ const ipWhitelist = require('../config/ipWhitelist.config');
 const axios = require('axios').default;
 
 class AdapterController {
-
+    //  tV - tradingView
     async determineStrategy(req, res, next) {
-
+        //  tradingViewData - data from tV webhook req
         const tradingViewData = req.body;
 
-        const ipFlag = ipWhitelist.some(ip => req.connection.remoteAddress.includes(ip))
+        //  Validate Ip address
+        const ipFlag = ipWhitelist.some(ip => req.connection.remoteAddress.includes(ip));
+
+        //  Determine strategy association with tV data.
+        //  Note: strategy.config should include such tradingViewData.strategy
         const currentStrategy = strategies.filter(strategy => strategy.strategy === tradingViewData.strategy);
 
         if (currentStrategy.length == 1 && ipFlag) {
 
             console.log(tradingViewData);
-            
+
+            //Rote tradingViewData to associated strategy server
             const [sendRequestError, sendRequest] = await to(
                 axios.post(`${currentStrategy.serverIp}:
                 ${tradingViewData.mode === "master" ? currentStrategy.master_port : currentStrategy.development_port}/alert_data`, tradingViewData)
