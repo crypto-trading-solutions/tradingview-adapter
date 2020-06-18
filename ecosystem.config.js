e2aa8f0dd22a5ca9a83059a4bcdb584924e2b6d7
Ivan <ivan.kodak.02@gmail.com>
2020-06-18 10:33:33 +0300

    Added logging to file at ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,11 @@ module.exports = {
         script: "./index.js",
         exec_mode: "cluster",
         watch: true,
+        error_file: 'adapterErr.log',
+        out_file: 'adapterOut.log',
+        log_file: 'adapter.log',
+        time: true,
+        watch: true,
         ignore_watch: ["node_modules"],
         env: {
           NODE_ENV: "development"
