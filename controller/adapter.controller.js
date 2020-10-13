d6f244160d9be6283a50cee125ddf2b7e299ae0a
DmitriyShalaev <shalaevdmitriy99@gmail.com>
2020-10-13 14:04:44 +0300

    use_close_calls_feature realization

--- controller/adapter.controller.js
+++ controller/adapter.controller.js
@@ -5,39 +5,64 @@ const ipWhitelist = require('../config/ipWhitelist.config');
 const axios = require('axios').default;
 
 class AdapterController {
+
     async determineStrategy(req, res, next) {
         //  tradingViewData - data from tV webhook req
         const tradingViewData = req.body;
 
-	    console.log("-----------tradingViewData----------------\n");
+        console.log("-----------tradingViewData----------------\n");
         console.log(tradingViewData);
         console.log("-----------tradingViewData----------------\n");
 
         //  Validate IP address
         const isTradingViewIp = ipWhitelist.some(ip => req.connection.remoteAddress.includes(ip));
 
-        //  Determine strategy association with tV data.
+        //  Determine strategy association with tradingView data.
         //  Note: strategy.config should include such tradingViewData.strategy
         const currentStrategy = strategies.filter(strategy => strategy.strategy_name === tradingViewData.strategy_name)[0];
 
         if (typeof currentStrategy !== 'undefined' && isTradingViewIp) {
+            //  Initiate a request to close a previous position, and then send an actual request to open a position.
+            if (currentStrategy.use_close_calls_feature == true) {
+                let close_data = tradingViewData;
 
-            //Rote tradingViewData to associated tradingView server
-            const [sendRequestError, sendRequest] = await to(
-                axios.post(`${currentStrategy.serverIp}:${tradingViewData.Mode === "master" ? currentStrategy.master_port : currentStrategy.development_port}/alert_data`, tradingViewData)
-            )
+                if (tradingViewData.Action == "long") {
+                    close_data.Action = "close_short";
+                }
+                else {
+                    close_data.Action = "close_long";
+                }
 
-            if (sendRequestError) {
-                console.log(sendRequestError);
-                return res.status(400).json(sendRequestError);
+                console.log("-----------close_data----------------\n");
+                console.log(close_data);
+                console.log("-----------close_data----------------\n");
+
+                sendStrategyExecutorCoreRequest(currentStrategy, close_data);
+                sendStrategyExecutorCoreRequest(currentStrategy, tradingViewData)
+            }
+            else {
+                sendStrategyExecutorCoreRequest(currentStrategy, tradingViewData)
             }
 
-            console.log(sendRequest.data);
-            res.status(200).json(sendRequest.data);
         }
         else {
             res.status(400).send(`Strategy: ${currentStrategy} ipFlag: ${isTradingViewIp}`);
         }
+    }
+
+    async sendStrategyExecutorCoreRequest(currentStrategy, tradingViewData) {
+        //Rote tradingViewData to associated tradingView server
+        const [sendRequestError, sendRequest] = await to(
+            axios.post(`${currentStrategy.serverIp}:${tradingViewData.Mode === "master" ? currentStrategy.master_port : currentStrategy.development_port}/alert_data`, tradingViewData)
+        )
+
+        if (sendRequestError) {
+            console.log(sendRequestError);
+            return res.status(400).json(sendRequestError);
+        }
+
+        console.log(sendRequest.data);
+        res.status(200).json(sendRequest.data);
     }
 }
 
